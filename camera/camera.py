987d8a63ab364fb526069e11122fa3c5cae22dae
David Klee <klee.d@northeastern.edu>
2021-03-30 15:26:10 -0400

    created checkerboard calibration data

--- camera/camera.py
+++ camera/camera.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import os
+import yaml
+import time
 
 #TODO:
 #   - handle error on connection, default to attempt reconnecting
@@ -15,7 +18,10 @@ class Camera:
 
         self._cam = self._connect()
 
-        self._calc_distortion_data()
+        self._configs = self._read_configs()
+        for k,v in self._configs.items():
+            if isinstance(v, list):
+                self._configs[k] = np.array(v)
 
         # self._get_camera_pose()
 
@@ -33,7 +39,6 @@ class Camera:
             return
 
         return image
-
 
     def reshape_image(self, image, height, width=None):
         if width is None:
@@ -53,61 +58,86 @@ class Camera:
         self._cam.release()
 
     def _calc_distortion_matrix(self):
-        image = self.get_image()
-        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        folder = "camera/checkerboard_calibration/"
+        positions = np.loadtxt(os.path.join(folder, "3dpositions.txt"),
+                                delimiter=',')
+        num_images = len(positions)
 
-        ret, corners = cv2.findChessboardCorners(gray, (7,9), None)
+        GW, GH = 7, 9
+        grid_size = 20 #mm
+        objp = np.zeros((GW*GH,3), np.float32)
+        objp[:,:2] = grid_size * np.mgrid[0:GW,0:GH].T.reshape(-1,2)
 
-        if ret:
-            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-            corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
-            print(corners.shape)
+        img_pts = []
+        obj_pts = []
+        for img_id in range(num_images):
+            img = cv2.imread(os.path.join(folder,f"{img_id}.jpg"))
+            gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-        plt.figure()
-        plt.imshow(gray, cmap='gray')
-        for x,y in corners[:,0]:
-            plt.plot(x-0.5,y-0.5,'r.')
-        plt.axis('off')
-        plt.show()
+            ret, corners = cv2.findChessboardCorners(gray, (GW,GH), None)
 
-    def _load_distortion_data(self):
-        print('loading non existant distortion data')
-        pass
+            if ret:
+                # criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+                # corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
+                img_pts.append(corners)
+                obj_pts.append(objp)
+
+        cv2.destroyAllWindows()
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_pts, img_pts, gray.shape[::-1], None, None)
+
+        h,  w = img.shape[:2]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+        self._write_configs({
+            'mtx' : mtx.tolist(),
+            'undistort_mtx' : newcameramtx.tolist(),
+            'undistort_roi' : roi,
+            'dist_coeffs' : dist.tolist(),
+        })
+
+    def _undistort(self, img):
+        # undistort
+        dst = cv2.undistort(img,
+                            self._configs['mtx'],
+                            self._configs['dist_coeffs'],
+                            None,
+                            self._configs['undistort_mtx']
+                           )
+        # crop the image
+        x, y, w, h = self._configs['undistort_roi']
+        cropped = dst[y:y+h, x:x+w]
+        return cropped
+
+    def _write_configs(self, new_configs):
+        doc = self._read_configs()
+
+        doc.update(new_configs)
+        with open("camera/configs.yaml", "w") as f:
+            yaml.safe_dump(doc, f, default_flow_style=False)
+
+    def _read_configs(self):
+        with open("camera/configs.yaml") as f:
+            doc = yaml.safe_load(f)
+
+        if doc is None:
+            doc = dict()
+
+        return doc
 
     def __del__(self):
         self._disconnect()
 
-def use_as_webcam(cam_id=0):
-    cam = cv2.VideoCapture(cam_id)
+    def live_feed(self, frate=30):
+        cv2.namedWindow("Live Feed")
+        while True:
+            img = self.get_image()
+            cv2.imshow("Live Feed", img)
+            cv2.waitKey(int(1000/frate))
 
-    cv2.namedWindow("test")
-
-    img_counter = 0
-
-    while True:
-        ret, frame = cam.read()
-        if not ret:
-            print("failed to grab frame")
-            break
-        cv2.imshow("test", frame)
-
-        k = cv2.waitKey(1)
-        if k%256 == 27:
-            # ESC pressed
-            print("Escape hit, closing...")
-            break
-        elif k%256 == 32:
-            # SPACE pressed
-            img_name = "opencv_frame_{}.png".format(img_counter)
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-            img_counter += 1
-
-    cam.release()
-
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     camera = Camera()
-    # use_as_webcam()
+    camera._calc_distortion_matrix()
+    camera.live_feed()
 
