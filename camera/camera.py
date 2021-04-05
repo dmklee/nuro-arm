8e134f7560e5fe0212012f2ec8df0d95d9607aba
David Klee <klee.d@northeastern.edu>
2021-04-05 17:01:26 -0400

    camera to world transformations can be calculated

--- camera/camera.py
+++ camera/camera.py
@@ -4,25 +4,27 @@ import cv2
 import yaml
 import time
 
-from camera.utils import calc_distortion_matrix
+from camera.utils import *
 #TODO:
 #   - handle error on connection, default to attempt reconnecting
-#   - write calc_camera_pose
+    # toggle between camera_ids
 
 class Camera:
     def __init__(self,
-                 camera_id=0,
+                 camera_id=1,
                  ):
         self._cam_id = camera_id
 
         self._cam = self._connect()
+
+        self._calc_distortion_matrix()
 
         self._configs = self._read_configs()
         for k,v in self._configs.items():
             if isinstance(v, list):
                 self._configs[k] = np.array(v)
 
-        # self._get_camera_pose()
+        self._get_ego_pose()
 
     def set_camera_id(self, camera_id):
         self._cam_id = camera_id
@@ -31,15 +33,19 @@ class Camera:
     def _connect(self):
         return cv2.VideoCapture(self._cam_id)
 
-    def get_image(self):
+    def _raw_image(self):
         ret, image = self._cam.read()
         if not ret:
             print('error, try reconnecting')
             return
-
         return image
 
-    def reshape_image(self, image, height, width=None):
+    def get_image(self):
+        img = self._raw_image()
+
+        return self._undistort(img)
+
+    def resize_image(self, image, height, width=None):
         if width is None:
             width = height
         reshaped = cv2.resize(image, (width, height))
@@ -65,6 +71,80 @@ class Camera:
             'dist_coeffs' : dist.tolist(),
         })
 
+    def _get_ego_pose(self):
+        if 'cam2world_rmat' in self._configs:
+            return
+
+        print('Calculating pose of camera...')
+        # vec that goes from world origin to where right suction cup touches grid
+        grid_tvec = np.array((70-6.5,77-6.5+20,0))
+        gh, gw = (7,9)
+        gsize = 20 # mm
+
+        img = self.get_image()
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+        ret, corners = cv2.findChessboardCorners(gray,
+                                                (gh,gw),
+                                                None)
+
+
+        if ret:
+            objp = np.zeros((gh*gw,3), np.float32)
+            objp[:,:2] = gsize * np.dstack(np.mgrid[1:-gw+1:-1,gh:0:-1]).reshape(-1,2)
+            objp += grid_tvec
+
+            mtx = self._configs['undistort_mtx']
+            dist = self._configs['dist_coeffs']
+
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+            corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
+            ret, rvecs, tvecs = cv2.solvePnP(objp, corners2, mtx, dist)
+
+            new_configs = {
+                'cam2world_tvec' : tvecs[:,0],
+                'cam2world_rmat' : cv2.Rodrigues(rvecs)[0],
+                'world2cam_tvec' : -tvecs[:,0],
+                'world2cam_rmat' : cv2.Rodrigues(-rvecs)[0]
+            }
+            self._configs.update(new_configs)
+            self._write_configs({k:v.tolist() for k,v in new_configs.items()})
+            return
+
+        print('ERROR: checkerboard pattern was not identified.')
+            # imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+
+            # fig = plt.figure()
+            # ax = plt.axes(projection='3d')
+            # origin = np.zeros(3)
+            # axis = 20*np.array(((1,0,0),(0,1,0),(0,0,1)))
+            # axis_colors = 'gbr'
+            # for a, c in zip(axis, axis_colors):
+                # x,y,z = zip(origin, a)
+                # ax.plot(x,y,z,'-', color=c)
+
+            # cam_rot_mat, jcb = cv2.Rodrigues(-rvecs)
+            # for a, c in zip(axis, axis_colors):
+                # rot_a = np.dot(cam_rot_mat, a)
+                # rot_vec = np.dot(cam_rot_mat, tvecs[:,0])
+                # x,y,z = zip(origin - rot_vec, rot_a - rot_vec)
+                # ax.plot(x,y,z,':', color=c)
+
+            # for a, c in zip(axis, axis_colors):
+                # rot_a = np.dot(world2grid_RMAT, a)
+                # x,y,z = zip(world2grid_TVEC, rot_a+world2grid_TVEC)
+                # ax.plot(x,y,z,'-', color=c)
+
+            # ax.plot(*objp.T, 'k.')
+
+            # plt.show()
+
+            # img = draw(img, corners2, imgpts)
+            # cv2.drawChessboardCorners(img, (7,9), corners2, ret)
+        # cv2.imshow('img', img)
+        # cv2.waitKey(5000)
+        # cv2.destroyAllWindows()
+
     def _undistort(self, img):
         # undistort
         dst = cv2.undistort(img,
@@ -85,6 +165,8 @@ class Camera:
         with open("camera/configs.yaml", "w") as f:
             yaml.safe_dump(doc, f, default_flow_style=False)
 
+        return doc
+
     def _read_configs(self):
         with open("camera/configs.yaml") as f:
             doc = yaml.safe_load(f)
@@ -101,14 +183,15 @@ class Camera:
         cv2.namedWindow("Live Feed")
         while True:
             img = self.get_image()
-            img = self._undistort(img)
             cv2.imshow("Live Feed", img)
             cv2.waitKey(int(1000/frate))
 
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    camera = Camera()
-    camera._calc_distortion_matrix()
-    camera.live_feed()
+    camera = Camera(2)
+
+    # calc_distortion_matrix()
+    # camera.calc_camera_pse()
+    # camera.live_feed()
 
--- camera/utils.py
+++ camera/utils.py
@@ -8,29 +8,34 @@ def get_cube_poses():
 def detect_blobs(color_thresholds):
     pass
 
-
 def calc_distortion_matrix():
-    folder = "camera/checkerboard_calibration/"
-    positions = np.loadtxt(os.path.join(folder, "3dpositions.txt"),
-                            delimiter=',')
-    num_images = len(positions)
+    folder = "camera/calibration_photos/photos/"
 
     GW, GH = 7, 9
-    grid_size = 1 #mm
+    grid_size = 20 #mm
     objp = np.zeros((GW*GH,3), np.float32)
     objp[:,:2] = grid_size * np.mgrid[0:GW,0:GH].T.reshape(-1,2)
 
     img_pts = []
     obj_pts = []
-    for img_id in range(num_images):
-        img = cv2.imread(os.path.join(folder,f"{img_id}.jpg"))
+    img_id = 0
+    while True:
+        img = cv2.imread(os.path.join(folder,f"{img_id}.png"))
+        if img is None:
+            break
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img_id += 1
 
         ret, corners = cv2.findChessboardCorners(gray, (GW,GH), None)
 
         if ret:
             img_pts.append(corners)
             obj_pts.append(objp)
+            # cv2.drawChessboardCorners(img, (GW,GH), corners, ret)
+            # cv2.imshow('img', img)
+            # cv2.waitKey(100)
+    # num_images = img_id
+    # cv2.destroyAllWindows()
 
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_pts,
                                                        img_pts,
@@ -38,9 +43,25 @@ def calc_distortion_matrix():
                                                        None,
                                                        None)
 
-    h,  w = img.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist,
-                                                      (w,h), 1,
-                                                      (640, 480))
+    h,  w = gray.shape
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,
+                                                      dist,
+                                                      (w,h),
+                                                      1,
+                                                      (w,h)
+                                                     )
+
+    # for img_id in range(num_images):
+        # img = cv2.imread(os.path.join(folder,f"{img_id}.png"))
+        # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+        # # crop the image
+        # x, y, w, h = roi
+        # dst = dst[y:y+h, x:x+w]
+        # cv2.drawChessboardCorners(img, (GW,GH), corners, True)
+        # cv2.imshow('img', dst)
+        # cv2.waitKey(500)
+    # cv2.destroyAllWindows()
+
 
     return mtx, newcameramtx, roi, dist
