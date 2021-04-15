3ef69ae48aa44775a3e4d24c59823b53192ed8f3
David Klee <klee.d@northeastern.edu>
2021-04-15 13:47:35 -0400

    basic collision detection by linear interpolation in joint space

--- neu_ro_arm/robot/motion_planner.py
+++ neu_ro_arm/robot/motion_planner.py
@@ -2,16 +2,16 @@ import numpy as np
 import pybullet as pb
 import pybullet_data
 
-from src.camera_utils import rotmat2euler
+from camera.camera_utils import rotmat2euler
 
 class MotionPlanner:
     '''Handles IK, FK, collision detection
     '''
-    ROBOT_URDF_PATH = "src/assets/xarm.urdf"
-    CAMERA_URDF_PATH = "src/assets/camera.urdf"
-    ROD_URDF_PATH = "src/assets/camera_rod.urdf"
-    def __init__(self):
-        self._init_pybullet()
+    ROBOT_URDF_PATH = "assets/urdf/xarm.urdf"
+    CAMERA_URDF_PATH = "assets/urdf/camera.urdf"
+    ROD_URDF_PATH = "assets/urdf/camera_rod.urdf"
+    def __init__(self, cam_pose_mtx=None):
+        self._init_pybullet(cam_pose_mtx)
         self.link_names = self._get_link_names()
         self.joint_names = self._get_joint_names()
         self.end_effector_link_index = self.link_names.index('hand')
@@ -34,24 +34,26 @@ class MotionPlanner:
         rot = pb.getEulerFromQuaternion(link_state[5])
         return pos, rot
 
-    def _init_pybullet(self):
+    def _init_pybullet(self, cam_pose_mtx):
         self._client = pb.connect(pb.GUI)
 
         # this path is where we find platform
         pb.setAdditionalSearchPath(pybullet_data.getDataPath())
-        pb.loadURDF('plane.urdf', [0,0.5,0])
+        self.plane_id = pb.loadURDF('plane.urdf', [0,0.5,0])
 
         # add xarm urdf
         self.robot_id = pb.loadURDF(self.ROBOT_URDF_PATH, [0,0,0],[0,0,0,1],
                                   flags=pb.URDF_USE_SELF_COLLISION)
 
         # initialize camera far away so collisions dont occur
+        if cam_pose_mtx is not None:
+            self.add_camera(cam_pose_mtx)
+
+    def add_camera(self, cam_pose_mtx):
         self.camera_id = pb.loadURDF(self.CAMERA_URDF_PATH, [0,0,0],[0,0,0,1])
         self.rod_id = pb.loadURDF(self.ROD_URDF_PATH, [0,0,0],[0,0,0,1])
-        self.rod_offset_vec = np.array((0.026, -0.012, -0.013))
 
-        T = np.load('src/configs/camera.npz')['cam2world']
-        self.set_camera_pose(T)
+        self.set_camera_pose(cam_pose_mtx)
 
     def _get_joint_names(self):
         num_joints = pb.getNumJoints(self.robot_id)
@@ -75,8 +77,21 @@ class MotionPlanner:
         return link_names
 
     def _teleport_arm(self, jpos):
-        [pb.resetJointState(self.id, i, jp)
+        [pb.resetJointState(self.robot_id, i, jp)
             for i,jp in zip(self.arm_joint_idxs, jpos)]
+
+    def check_arm_trajectory(self, target_jpos, num_steps=10):
+        '''Checks collision of arm links'''
+        assert len(target_jpos) == len(self.arm_joint_idxs)
+        current_jpos = np.array([pb.getJointState(self.robot_id, j_idx)[0]
+                               for j_idx in self.arm_joint_idxs])
+        inter_jpos = np.linspace(current_jpos, target_jpos, num=num_steps, endpoint=True)
+
+        for jpos in inter_jpos[1:]:
+            if self._check_collisions(jpos):
+                return False
+
+        return True
 
     def _monitor_movement(self, j_idxs, target, read_fn, max_iter=100, atol=1e-4):
         it = 0
@@ -93,18 +108,41 @@ class MotionPlanner:
 
         return True
 
+    def _check_collisions(self, jpos):
+        self._teleport_arm(jpos)
+        pb.performCollisionDetection(self._client)
+        for cont_pt in pb.getContactPoints(physicsClientId=self._client):
+            bodies = (cont_pt[1], cont_pt[2])
+            links = (cont_pt[3], cont_pt[4])
+            if self.robot_id in bodies:
+                body_idx = bodies.index(self.robot_id)
+                robot_link = links[body_idx]
+                if self.link_names[robot_link] != 'base':
+                    return True
+
+        return False
+
     def set_camera_pose(self, cam2world):
         pos = cam2world[:3,3]/1000.
         rotmat = cam2world[:3,:3]
         quat = pb.getQuaternionFromEuler(rotmat2euler(rotmat))
         pb.resetBasePositionAndOrientation(self.camera_id, pos, quat)
 
-        rod_pos = pos + np.dot(rotmat, self.rod_offset_vec)
+        rod_offset_vec = np.array((0.026, -0.012, -0.013))
+        rod_pos = pos + np.dot(rotmat, rod_offset_vec)
         rod_pos[2] = 0
         pb.resetBasePositionAndOrientation(self.rod_id, rod_pos, (0,0,0,1))
 
 if __name__ == "__main__":
-    mp = MotionPlanner()
+    import constants as constants
+    mp = MotionPlanner(constants.default_cam_pose_mtx)
+    jpos = np.array([0,0,-np.pi/2,0,0])
+    mp._teleport_arm(jpos)
+    target = np.array([0,np.pi,-np.pi/2,0,0])
+    print(mp.check_arm_trajectory(target, num_steps=2))
+    sadf
     import time
     while True:
-        time.sleep(1)
+        print(mp._get_collisions_at(jpos))
+        pb.stepSimulation()
+        time.sleep(0.1)
