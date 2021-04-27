cb5a68e63cb174067a33b8da0201e3f1226ad4b6
David Klee <klee.d@northeastern.edu>
2021-04-27 13:54:59 -0400

    walkthrough for week1

---
+++ class_material/week1/read_joint_positions.py
@@ -0,0 +1,45 @@
+##############################################################################
+# Author: David Klee
+# Date: 4.27.21
+#
+# The purpose of this script is to print measurements of the robots arm joint
+# positions, while it is allowed to be passively moved around.  The printing
+# will continue until the robot gripper is closed by the user
+#
+##############################################################################
+import time
+from neu_ro_arm.robot.robot_arm import RobotArm
+from neu_ro_arm.constants import GRIPPER_CLOSED
+
+# Connect to the robot
+robot_mode = 'real'
+robot = RobotArm(robot_mode)
+
+# enter passive mode so motors move freely
+robot.passive_mode()
+
+# loop will run until gripper is closed manually
+robot.open_gripper()
+while True:
+    # get arm joint positions in radians
+    arm_jpos = robot.get_arm_jpos()
+
+    # log the joint positions to the terminal
+    for i in range(len(arm_jpos)):
+        joint_name = robot.joint_names[i]
+        joint_pos = arm_jpos[i]
+        print(f"{joint_name} : {joint_pos:.2f} radians")
+
+    print()
+
+    # check for termination condition
+    gripper_state = robot.get_gripper_state()
+    if gripper_state < GRIPPER_CLOSED + 0.01:
+        break
+
+    # add some delay so the while loop doesnt run too fast
+    delay = 0.1 # seconds
+    time.sleep(delay)
+
+print('Program is ending')
+
