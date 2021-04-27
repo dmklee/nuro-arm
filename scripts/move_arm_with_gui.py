789e1a6305de6259981c7bfc782c106b1229ca77
David Klee <klee.d@northeastern.edu>
2021-04-27 10:29:36 -0400

    gui with cube in simulated version

--- scripts/move_arm_with_gui.py
+++ scripts/move_arm_with_gui.py
@@ -8,11 +8,16 @@ def move_with_gui(robot_mode):
     Currently, there is no collision detection during gui motions so the user
     should be careful and ensure no objects are within the reach of the arm
 
+    In simulator, a cube will be added just for fun
+
     Parameters
     ----------
     robot_mode : {'real', 'sim'}
     '''
     robot = RobotArm(robot_mode)
+    if robot_mode == 'sim':
+        robot.controller.add_cube(np.array((0.01, 0.2, 0.015)),
+                                  np.zeros(3) )
     robot.move_with_gui()
 
 if __name__ == "__main__":
@@ -23,6 +28,5 @@ if __name__ == "__main__":
 
     assert robot_mode in ('real','sim'), 'Invalid robot mode argument'
 
-    robot = RobotArm(robot_mode)
-    robot.move_with_gui()
+    move_with_gui(robot_mode)
 
