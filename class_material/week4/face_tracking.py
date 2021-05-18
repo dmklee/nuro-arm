2ac23a790636cbc08ff0de8ebfb618a1cf98bab6
David Klee <klee.d@northeastern.edu>
2021-05-18 15:05:09 -0400

    asdf

--- class_material/week4/face_tracking.py
+++ class_material/week4/face_tracking.py
@@ -7,26 +7,13 @@
 import numpy as np
 import cv2
 import time
+from threading import Thread
 
 from neu_ro_arm.camera.camera import Camera
 from neu_ro_arm.robot.robot_arm import RobotArm
 from neu_ro_arm.camera.camera_utils import find_face
 from neu_ro_arm.camera.gui import ShowFace
 
-camera = Camera()
-
-# if you want to visualize the face detection, uncomment this section
-# script will hand on this function until you close window
-# camera.gui.show(window_name='Detecting Faces (press ESC to exit)',
-               # modifier_fns=[ShowFace(camera.unpack_configs(False))])
-
-
-# initialize the robot
-starting_arm_jpos = [0.0, -0.85, 1.0, 1.24, 0.0]
-robot = RobotArm()
-robot.move_arm_jpos(starting_arm_jpos)
-
-# create function for relative movement of base motor
 def move_base_relative(robot, rel_angle):
     '''Moves base motor by some relative angle (in radians)
     '''
@@ -39,30 +26,58 @@ def move_base_relative(robot, rel_angle):
     # move to adjusted postion
     robot.move_arm_jpos(arm_jpos)
 
-
-# Face Tracking code begins here
-delay = 0.5
-while True:
-    time.sleep(delay)
-
-    # get image from camera
-    image = camera.get_image()
-
-    # detect face
-    face = find_face(image)
-    if face is None:
-        # no face found so go to next iteration
-        continue
-
-    # unpack info about face location and shape
+def show_image(image, face, duration=500):
+    '''Moves base motor by some relative angle (in radians)
+    '''
     x,y,w,h = face
+    cv2.ellipse(image, (x, y), (int(w/2), int(h/2)),
+                 0, 0, 360, color=(0,0,255), thickness=4)
+    cv2.imshow('', image)
+    cv2.waitKey(duration)
+    cv2.destroyAllWindows()
 
-    #####################################
-    # your code goes here
-    #####################################
-    move_base_relative(robot, -x/600)
+if __name__ == "__main__":
+    camera = Camera()
 
-    #####################################
-    #####################################
+    # if you want to visualize the face detection, uncomment this section
+    # script will hand on this function until you close window
+    draw_face = ShowFace(camera.unpack_configs(False))
+    # camera.gui.show(window_name='Detecting Faces (press ESC to exit)',
+                   # modifier_fns=[draw_face])
+
+
+    # initialize the robot
+    starting_arm_jpos = [0.0, -0.85, 1.0, 1.24, 0.0]
+    robot = RobotArm()
+    robot.move_arm_jpos(starting_arm_jpos)
+
+    # Face Tracking code begins here
+    delay = 0.5
+    while True:
+        time.sleep(delay)
+
+        # get image from camera
+        image = camera.get_image()
+
+        # detect face
+        face = find_face(image)
+        if face is None:
+            # no face found so go to next iteration
+            continue
+
+        # visualize the face, helpful for debugging
+        show_image(image, face)
+
+        # unpack info about face location and shape
+        x,y,w,h = face
+
+        #####################################
+        # your code goes here
+        #####################################
+        cx, cy = img.shape/2
+        move_base_relative(robot, -(x-cx)/600)
+
+        #####################################
+        #####################################
 
 
