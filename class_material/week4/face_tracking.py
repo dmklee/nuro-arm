0fe03cecfbfb096e8eb593b350bee392fd7f2792
David Klee <klee.d@northeastern.edu>
2021-05-18 14:47:11 -0400

    import missing

--- class_material/week4/face_tracking.py
+++ class_material/week4/face_tracking.py
@@ -6,6 +6,7 @@
 ##############################################################################
 import numpy as np
 import cv2
+import time
 
 from neu_ro_arm.camera.camera import Camera
 from neu_ro_arm.robot.robot_arm import RobotArm
