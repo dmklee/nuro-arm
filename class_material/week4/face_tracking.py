0dba653aac4f047187fda77e86b779b0e6f3ca5e
David Klee <klee.d@northeastern.edu>
2021-05-21 19:24:10 -0400

    improved so gui is visible the whole time

--- class_material/week4/face_tracking.py
+++ class_material/week4/face_tracking.py
@@ -14,66 +14,72 @@ from neu_ro_arm.robot.robot_arm import RobotArm
 from neu_ro_arm.camera.camera_utils import find_face
 from neu_ro_arm.camera.gui import ShowFace
 
-def move_base_relative(robot, rel_angle):
-    '''Moves base motor by some relative angle (in radians)
+class FaceFollower(Thread):
+    def __init__(self, robot, cam):
+        super().__init__()
+        self.robot = robot
+        self.camera = camera
+        self.delay = 0.3 # seconds
+        self.running = True
+        self.BASE_ID = 0
+        self.WRIST_ID = 3
+
+    def run(self):
+        while self.running:
+            image = self.camera.get_image()
+
+            # detect face in image
+            face = find_face(image)
+            if face is None:
+                # no face found so go to next iteration
+                time.sleep(self.delay)
+                continue
+
+            # call the function that you wrote
+            rel_base_move, rel_wrist_move = follow_face(face, image)
+
+            # issue relative motion commands
+            arm_jpos = self.robot.get_arm_jpos()
+            arm_jpos[self.BASE_ID] += rel_base_move
+            arm_jpos[self.WRIST_ID] += rel_wrist_move
+            self.robot.move_arm_jpos(arm_jpos)
+
+def follow_face(detected_face, image):
+    '''Returns relative movement commands
+
+    You may want to add print statements to help you out here
     '''
-    base_servo_idx = 0
-    arm_jpos = robot.get_arm_jpos()
+    x, y, w, h = detected_face
+    image_height, image_width, _ = image.shape
 
-    # adjust the angle of the base servo
-    arm_jpos[base_servo_idx] += rel_angle
+    rel_wrist_move = 0
+    rel_base_move = 0
+    #####################################
+    # your code goes here
+    #####################################
 
-    # move to adjusted postion
-    robot.move_arm_jpos(arm_jpos)
-
-def show_image(image, face, duration=500):
-    '''Moves base motor by some relative angle (in radians)
-    '''
-    x,y,w,h = face
-    cv2.ellipse(image, (x, y), (int(w/2), int(h/2)),
-                 0, 0, 360, color=(0,0,255), thickness=4)
-    cv2.imshow('', image)
-    cv2.waitKey(duration)
-    cv2.destroyAllWindows()
+    #####################################
+    #####################################
+    return rel_base_move, rel_wrist_move
 
 if __name__ == "__main__":
+    # initialize the camera
     camera = Camera()
-
-    # if you want to visualize the face detection, uncomment this section
-    # script will hand on this function until you close window
-    draw_face = ShowFace(camera.unpack_configs(False))
-    # camera.gui.show(window_name='Detecting Faces (press ESC to exit)',
-                   # modifier_fns=[draw_face])
-
 
     # initialize the robot
     starting_arm_jpos = [0.0, -0.85, 1.0, 1.24, 0.0]
     robot = RobotArm()
     robot.move_arm_jpos(starting_arm_jpos)
 
-    # Face Tracking code begins here
-    delay = 0.5
-    while True:
-        time.sleep(delay)
+    # begin thread that issues commands based on your code
+    follower = FaceFollower(robot, camera)
+    follower.start()
 
-        # get image from camera
-        image = camera.get_image()
+    # set up GUI so you can visualize the face in the image
+    draw_face = ShowFace(camera.unpack_configs(False))
+    camera.gui.show(window_name='Detecting Faces (press ESC to exit)',
+                   modifier_fns=[draw_face])
 
-        # detect face
-        face = find_face(image)
-        if face is None:
-            # no face found so go to next iteration
-            continue
-
-        # visualize the face, helpful for debugging
-        show_image(image, face)
-
-        # unpack info about face location and shape
-        x,y,w,h = face
-
-        #####################################
-        # your code goes here
-        #####################################
-
-        #####################################
-        #####################################
+    # close follower so robot stops moving
+    follower.running = False
+    time.sleep(0.5)
