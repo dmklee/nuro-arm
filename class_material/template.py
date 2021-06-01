98e2b6ae5ca99c57f0b2a10068041fc3d57484da
David Klee <klee.d@northeastern.edu>
2021-06-01 12:05:59 -0400

    added tests for debugging cube picking

---
+++ class_material/template.py
@@ -0,0 +1,16 @@
+import time
+
+from neu_ro_arm.robot.robot_arm import RobotArm
+from neu_ro_arm.camera.camera import Camera
+
+# define functions here
+
+
+if __name__ == "__main__":
+    # put any code you want here
+
+    # connect to robot
+    robot = RobotArm()
+
+    # connect to camera
+    camera = Camera()
