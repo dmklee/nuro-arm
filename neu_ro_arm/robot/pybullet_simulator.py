5af2c3373d036d207c89d6ace9e3315da6280948
David Klee <klee.d@northeastern.edu>
2021-07-15 09:24:55 -0400

    refactored pybullet simulator so it can be used by controller and motion planner concurrently

--- neu_ro_arm/robot/pybullet_simulator.py
+++ neu_ro_arm/robot/pybullet_simulator.py
@@ -3,12 +3,12 @@ import pybullet as pb
 import numpy as np
 from neu_ro_arm import transformation_utils
 
-class BasePybullet:
+class PybulletSimulator:
     ROBOT_URDF_PATH = "neu_ro_arm/assets/urdf/xarm.urdf"
     CAMERA_URDF_PATH = "neu_ro_arm/assets/urdf/camera.urdf"
     ROD_URDF_PATH = "neu_ro_arm/assets/urdf/camera_rod.urdf"
     CUBE_URDF_PATH = "neu_ro_arm/assets/urdf/cube.urdf"
-    def __init__(self, connection_mode):
+    def __init__(self, headless):
         '''Base class for pybullet simulator to handle initialization and attributes
         about robot joints
 
@@ -22,9 +22,9 @@ class BasePybullet:
         end_effector_link_index : int
             index of hand link, this is used to specify which link
             should be used for IK
-        arm_joint_idxs: list of int
+        arm_joint_ids: list of int
             indices of joints that control the arm
-        gripper_joint_idxs: list of int
+        gripper_joint_ids: list of int
             indices of two gripper joints. for simulator, gripper operation is
             controlled by two joints which should both move in concert to reflect
             the real xArm
@@ -37,22 +37,38 @@ class BasePybullet:
         camera_exists : bool
             True if camera collision object has been added to simulator
         '''
-        self.arm_joint_idxs = [1,2,3,4,5]
-        self.linkage_joint_idxs = [6,9]
-        self.gripper_joint_idxs = [7,10]
-        self.finger_joint_idxs = [8,11]
+        self.arm_joint_ids = [1,2,3,4,5]
+        self.linkage_joint_ids = [6,9]
+        self.gripper_joint_ids = [7,10]
+        self.finger_joint_ids = [8,11]
         self.end_effector_link_index = 12
 
-        self._init_pybullet(connection_mode)
+        self.arm_joint_limits = np.array(((-2, -1.58, -2, -1.8, -2),
+                                          ( 2,  1.58,  2,  2.0,  2)))
 
-        self.gripper_closed = np.array([0.05, 0.05])
-        self.gripper_opened = np.array([1.38, 1.38])
+        self.gripper_joint_limits = np.array([0.05, 1.38])
+
+        connection_mode = pb.DIRECT if headless else pb.GUI
+        self._init_pybullet(connection_mode)
+        self.n_joints = pb.getNumJoints(self.robot_id,
+                                          physicsClientId=self._client)
+
+        self.joint_names = []
+        self.link_names = []
+        self.joint_ll = []
+        self.joint_ul = []
+        for j_id in range(self.n_joints):
+            j_info = pb.getJointInfo(self.robot_id, j_id, physicsClientId=self._client)
+            self.joint_names.append(j_info[1].decode('ascii'))
+            self.joint_ll.append(j_info[8])
+            self.joint_ul.append(j_info[9])
+            self.link_names.append(j_info[12].decode('ascii'))
+        self.joint_ll = np.array(self.joint_ll)
+        self.joint_ul = np.array(self.joint_ul)
+
         self.camera_exists = False
 
-        self.link_names = self._get_link_names()
-        self.num_joints = pb.getNumJoints(self.robot_id, physicsClientId=self._client)
-
-    def _init_pybullet(self, connection_mode):
+    def _initialize(self, connection_mode):
         '''Creates pybullet simulator and loads world plane and robot.
 
         Parameters
@@ -73,52 +89,98 @@ class BasePybullet:
         self.plane_id = pb.loadURDF('plane.urdf', [0,0.5,0],
                                     physicsClientId=self._client)
 
-        self.robot_id = pb.loadURDF(self.ROBOT_URDF_PATH, [0,0,0],[0,0,0,1],
+        suction_cup_height = 0.012
+        self.robot_id = pb.loadURDF(self.ROBOT_URDF_PATH,
+                                    [0,0,suction_cup_height],
+                                    [0,0,0,1],
                                     flags=pb.URDF_USE_SELF_COLLISION,
                                     physicsClientId=self._client)
         for i in [0,1]:
-            pb.resetJointState(self.robot_id, self.gripper_joint_idxs[i], np.pi/2)
-            pb.resetJointState(self.robot_id, self.linkage_joint_idxs[i], np.pi/2)
+            pb.resetJointState(self.robot_id, self.gripper_joint_ids[i], np.pi/2)
+            pb.resetJointState(self.robot_id, self.linkage_joint_ids[i], np.pi/2)
 
         # set up constraints for linkage in gripper fingers
         left_linkage_constraint = pb.createConstraint(self.robot_id,
-                                                      self.linkage_joint_idxs[0],
+                                                      self.linkage_joint_ids[0],
                                                       self.robot_id,
-                                                      self.finger_joint_idxs[0],
+                                                      self.finger_joint_ids[0],
                                                       pb.JOINT_POINT2POINT,
                                                       (0,0,0),
                                                       (0.03,0,0.0),
                                                       (-0.022,0.0,0.0),
                                                       physicsClientId= self._client
                                                      )
-        pb.changeConstraint(left_linkage_constraint, maxForce=10_000)
+        pb.changeConstraint(left_linkage_constraint, maxForce=10000)
         right_linkage_constraint = pb.createConstraint(self.robot_id,
-                                                       self.linkage_joint_idxs[1],
+                                                       self.linkage_joint_ids[1],
                                                        self.robot_id,
-                                                       self.finger_joint_idxs[1],
+                                                       self.finger_joint_ids[1],
                                                        pb.JOINT_POINT2POINT,
                                                        (0,0,1),
                                                        (-0.03,0,0.0),
                                                        (0.022,0.0,0.0),
                                                        physicsClientId= self._client
                                                       )
-        pb.changeConstraint(right_linkage_constraint, maxForce=10_000)
+        pb.changeConstraint(right_linkage_constraint, maxForce=10000)
 
         # allow finger and linkages to move freely
         pb.setJointMotorControlArray(self.robot_id,
-                                     self.linkage_joint_idxs+self.finger_joint_idxs,
+                                     self.linkage_joint_ids+self.finger_joint_ids,
                                      pb.POSITION_CONTROL,
                                      forces=[0,0,0,0])
 
         # make arm joints rigid
         pb.setJointMotorControlArray(self.robot_id,
-                                     self.arm_joint_idxs,
+                                     self.arm_joint_ids,
                                      pb.POSITION_CONTROL,
                                      5*[0],
                                      positionGains=5*[0.1],
                                     )
 
         pb.stepSimulation(self._client)
+
+    def get_hand_pose(self):
+        '''Get position and orientation of hand (i.e. where grippers would close)
+
+        This is not the same as the hand link, instead we are interested in the
+        space where the grippers would engage with an object
+
+        Returns
+        -------
+        ndarray
+            position vector; shape=(3,); dtype=float
+        ndarray
+            euler angle; shape=(3,); dtype=float
+        '''
+        link_state = pb.getLinkState(self.robot_id,
+                                     self.end_effector_link_index,
+                                     physicsClientId=self._client)
+        pos = link_state[4]
+        rot = pb.getEulerFromQuaternion(link_state[5])
+        return pos, rot
+
+    def _get_link_pose(self, link_name):
+        '''Returns position and orientation of robot's link
+
+        Parameters
+        ----------
+        link_name: str
+            name of link in urdf. it should not include "_link" at end of name
+
+        Returns
+        -------
+        pos: ndarray
+            3D position; shape=(3,); dtype=float
+        rot: ndarray
+            euler angle; shape=(3,); dtype=float
+        '''
+        assert link_name in self.link_names
+        link_index = self.link_names.index(link_name)
+        link_state = pb.getLinkState(self.robot_id, link_index,
+                                     physicsClientId=self._client)
+        pos = link_state[4]
+        rot = pb.getEulerFromQuaternion(link_state[5])
+        return pos, rot
 
     def add_camera(self, pose_mtx):
         '''Adds or moves collision object to simulator where camera is located.
@@ -200,57 +262,5 @@ class BasePybullet:
                              )
         return cube_id
 
-    def _get_link_pose(self, link_name):
-        '''Returns position and orientation of robot's link
-
-        Parameters
-        ----------
-        link_name: str
-            name of link in urdf. it should not include "_link" at end of name
-
-        Returns
-        -------
-        pos: ndarray
-            3D position; shape=(3,); dtype=float
-        rot: ndarray
-            euler angle; shape=(3,); dtype=float
-        '''
-        assert link_name in self.link_names
-        link_index = self.link_names.index(link_name)
-        link_state = pb.getLinkState(self.robot_id, link_index,
-                                     physicsClientId=self._client)
-        pos = link_state[4]
-        rot = pb.getEulerFromQuaternion(link_state[5])
-        return pos, rot
-
-    def get_hand_pose(self):
-        '''Get position and orientation of hand (i.e. where grippers would close)
-
-        This is not the same as the hand link, instead we are interested in the
-        space where the grippers would engage with an object
-
-        Returns
-        -------
-        ndarray
-            position vector; shape=(3,); dtype=float
-        ndarray
-            euler angle; shape=(3,); dtype=float
-        '''
-        link_state = pb.getLinkState(self.robot_id,
-                                     self.end_effector_link_index,
-                                     physicsClientId=self._client)
-        pos = link_state[4]
-        rot = pb.getEulerFromQuaternion(link_state[5])
-        return pos, rot
-
-    def _get_link_names(self):
-        '''Returns list of names for each link in robot urdf
-        '''
-        num_joints = pb.getNumJoints(self.robot_id, physicsClientId=self._client)
-
-        link_names = [pb.getJointInfo(self.robot_id, j_idx, physicsClientId=self._client)[12]
-                           for j_idx in range(num_joints)]
-
-        link_names = [name.decode("utf-8").replace('_link','') for name in link_names]
-
-        return link_names
+    def close(self):
+        pb.disconnect(self._client)
