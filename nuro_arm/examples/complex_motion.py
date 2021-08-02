ac1b2560068dc0cad3caee0b90fb6a534d040967
David Klee <klee.d@northeastern.edu>
2021-08-02 11:31:34 -0400

    example showing how to achieve complex joint motions

--- nuro_arm/examples/complex_motion.py
+++ nuro_arm/examples/complex_motion.py
@@ -1,22 +1,53 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
-import time
+import pybullet as pb
 
 from nuro_arm.robot.robot_arm import RobotArm
 from nuro_arm.robot.xarm_controller import itos
 
-robot = RobotArm()
-robot.passive_mode()
-print('here')
-time.sleep(0.5)
+# generate circular path to follow
+N = 100
+R = 0.07
+angles = np.linspace(0, 2*np.pi, num=N, endpoint=True)
+waypts = np.zeros((N, 3))
+waypts[:,0] = 0.15
+waypts[:,1] = R * np.sin(angles)
+waypts[:,2] = R * np.cos(angles) + 0.2
 
-joint = 'base'
-j_id = robot.controller.get_joint_id(joint)
+##########################
+# simulator to calculate ik
+##########################
+robot = RobotArm('sim', headless=False, realtime=False)
+client = robot.mp.get_client()
+yaw, pitch, _, target = pb.getDebugVisualizerCamera()[-4:]
+pb.resetDebugVisualizerCamera(0.75, yaw, pitch, target)
 
-angles = np.linspace(0, 2*np.pi, num=200)
-vals = np.pi/2 * np.sin(angles)
+arm_jposs = []
+robot.move_hand_to(waypts[0])
+for i in range(len(waypts)):
+    pb.addUserDebugLine(waypts[i-1], waypts[i],
+                        [0.8,0.3,0.2],
+                        physicsClientId=client)
+    arm_jpos, _ = robot.mp.calculate_ik(waypts[i])
+    arm_jposs.append(arm_jpos)
+    robot.move_arm_jpos(arm_jpos)
+time.sleep(2)
+pb.disconnect(client)
 
+##########################
+# follow arm_jposs on real arm
+##########################
+robot = RobotArm('real')
+
+robot.move_arm_jpos(arm_jposs[0])
+
+obs_hand_pos = []
+joint_ids = robot.controller.arm_joint_ids
 while 1:
-    for v in vals:
-        robot.controller._move_servo(j_id, v, duration=20)
-        # time.sleep(0.0)
+    for arm_jpos in arm_jposs:
+        robot.controller.move_servos(joint_ids, arm_jpos, duration=15)
+        time.sleep(0.015)
+
+time.sleep(2)
+
