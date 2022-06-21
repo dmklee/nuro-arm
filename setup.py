c150289d0553bfe2cdc0b0c9d4dbdc9c4b2c0988
David Klee <klee.d@northeastern.edu>
2022-06-21 11:53:31 -0400

    cleaning it up; removed dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setup(
     name='nuro-arm',
     version='0.0.1',
-    description='NU Robotics Outreach: Learning Robotic Manipulation',
+    description='NURO Arm: Accessible Robotics Educational Platform',
+    license="MIT License",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='David Klee',
     author_email='klee.d@northeastern.edu',
     url='https://github.com/dmklee/nuro-arm',
@@ -13,13 +19,17 @@ setup(
     setup_requires="wheel",
     install_requires=[
         "numpy",
+        "scipy",
         "pybullet>=3.1.7",
-        "matplotlib",
         "easyhid;platform_system=='Linux'",
         "hid;platform_system=='Windows'",
         "hidapi;platform_system=='Darwin'",
-        "sklearn",
-        "scipy",
-        "gym",
+    ],
+
+    keywords=[
+        'robotics',
+        'educational-project',
+        'low-cost-robot',
+        'robotic-manipulation',
     ],
 )
