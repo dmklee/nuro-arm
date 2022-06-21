a95cdc2069947490357aceb74c9fe9aee74de646
David Klee <klee.d@northeastern.edu>
2022-06-21 12:03:17 -0400

    removed unnecessary cv2 import; easier interface for calibration

--- nuro_arm/__init__.py
+++ nuro_arm/__init__.py
@@ -2,10 +2,5 @@ from nuro_arm.robot.robot_arm import RobotArm
 from nuro_arm.camera.camera import Camera
 from nuro_arm.cube import Cube
 
-from gym.envs.registration import register
-
-register(
-    id='nuroarm-simple-picking-v0',
-    entry_point='nuro_arm.examples.gym_envs.simple_picking_env:ImageBasedPickingEnv',
-    max_episode_steps=5
-)
+from nuro_arm.robot.calibrate import calibrate_xarm
+from nuro_arm.camera.calibrate import calibrate_camera
--- nuro_arm/transformation_utils.py
+++ nuro_arm/transformation_utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 from scipy.spatial.transform import Rotation as R
 
 #ToDo: add docstrings
